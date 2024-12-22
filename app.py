e4b41cf026e5a675244cc56824f0606fefbd91be
Salma-Muhammad-Elnady <salmaelnady23@gmail.com>
2024-12-22 14:38:25 +0200

    first commit

---
+++ app.py
@@ -0,0 +1,42 @@
+from flask import Flask, render_template, request
+import joblib
+import numpy as np
+
+app = Flask(__name__)
+model = joblib.load('best_xgboost.pkl')
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    if request.method == 'POST':
+        try:
+            Glucose = int(request.form['Glucose'])
+            BloodPressure = int(request.form['BloodPressure'])
+            SkinThickness = int(request.form['SkinThickness'])
+            Insulin = int(request.form['Insulin'])
+            BMI = float(request.form['BMI'])
+            DiabetesPedigreeFunction = float(request.form['DiabetesPedigreeFunction'])
+            Age = int(request.form['Age'])
+
+            # تشكيل بيانات الإدخال
+            data = np.array([[Glucose, BloodPressure, SkinThickness, Insulin, BMI, DiabetesPedigreeFunction, Age]])
+
+            # التنبؤ باستخدام النموذج
+            prediction = model.predict(data)
+            if prediction[0] == 1:
+                result = 'The patient is infected'
+                result_class = 'infected'  
+            else:
+                result = 'Not infected'
+                result_class = 'not-infected'
+            
+
+            return render_template('result.html', prediction=result)
+        except ValueError as e:
+            return render_template('result.html', prediction="Please enter valid numeric values.", error=str(e))
+
+if __name__ == '__main__':
+    app.run(debug=True)
